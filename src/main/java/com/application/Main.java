e997d5396943c06278bf1acfc13fb63320724312
pintordev <pintor.dev@gmail.com>
2023-05-11 12:37:57 +0900

    greedy problem-ing

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -6,33 +6,28 @@ public class Main {
     public static void main(String[] args) {
         // 문제 테스트 데이터
         int n = 5;
-        int[] lost = {1, 3};
-        int[] reserve = {2, 4};
-        // 0 2 0 2 1
-        // 우선 순위?
-        // 양쪽 다 빌릴 수 있다 => 순위 낮음
-        // 한쪽만 빌릴 수 있다 => 순위 높음
+        int[] lost = {4, 2};
+        int[] reserve = {3, 5};
+
+        // 1 1 1 1 0 2
+
         int result = 0;
-        // 여벌 있을 경우 대여, 앞이나 뒤만 가능
-        // 최대한 많은 학생
-        // lost reserve 로 전체 학생들의 체육복 보유를 나타내는 배열 선언?
         int[] now = new int[n];
 
         for (int i = 0; i < n; i++) {
             now[i] = calNow(isLost(i + 1, lost), isReserve(i + 1, reserve));
         }
-        for (int i = 0; i < n - 1; i++) {
-            if (now[i] == 2 && now[i + 1] == 0) {
-                now[i] = 1;
-                now[i + 1] = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (now[i] == 2) {
+                int wPriority = priority(i, now);
+                if (wPriority != 0) {
+                    now[i + wPriority] = 1;
+                    now[i] = 1;
+                }
             }
         }
-        for (int i = n - 1; i > 0; i--) {
-            if (now[i] == 2 && now[i - 1] == 0) {
-                now[i] = 1;
-                now[i - 1] = 1;
-            }
-        }
+
         for (int i = 0; i < n; i++) {
             if (now[i] > 0) result++;
         }
@@ -63,6 +58,32 @@ public class Main {
             return 1;
         } else {
             return 2;
+        }
+    }
+
+    public static int priority(int i, int[] now) {
+        int previousPriority = calPriority(i - 1, now);
+        int nextPriority = calPriority(i + 1, now);
+
+        if (previousPriority == 1) return -1;
+        if (nextPriority == 1) return 1;
+        if (previousPriority == 2 && nextPriority == 2) return -1;
+        return 0;
+    }
+
+    public static int calPriority(int i, int[] now) {
+        int count = 0;
+        if (i >= 0 && i < now.length) {
+            if (now[i] != 0) return 0;
+            if (i > 0) {
+                if (now[i - 1] == 2) count++;
+            }
+            if (i < now.length - 1) {
+                if (now[i + 1] == 2) count++;
+            }
+            return count;
+        } else {
+            return 0;
         }
     }
 }
